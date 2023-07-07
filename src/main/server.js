3af57bd99f422ad06833362ea9f4698835f58ff6
Ruyter Araujo <ruyteraraujo992@gmail.com>
2023-07-07 13:21:45 -0300

    fix

--- src/main/server.js
+++ src/main/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dataBase = require("./database/db");
+const cors = require("cors");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
 
 const uploadRoutes = require("./routes/uploadRoutes.js");
 const userRoutes = require("./routes/userRoutes");
